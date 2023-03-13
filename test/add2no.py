9c7aaff1d797b13ffe1bc6ecb0caffa6c063555a
SAI <saigujral711@gmail.com>
2023-03-13 15:35:25 +0530

    test file added

---
+++ test/add2no.py
@@ -0,0 +1,3 @@
+
+def add2no(a: int, b: int) -> int:
+    return a + b
---
+++ test/test_add2no.py
@@ -0,0 +1,12 @@
+
+from add2no import add2no
+
+def test_add2no():
+    assert add2no(2, 3) == 5
+    assert add2no(0, 0) == 0
+    assert add2no(-2, 3) == 1
+    assert add2no(4,7)==11
+
+
+
+
